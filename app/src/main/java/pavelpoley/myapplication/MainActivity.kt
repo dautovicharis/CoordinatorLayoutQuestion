607aa6f09d40fae9043d4c5c734003fdb259ee09
pavel <pavelpoley88@gmail.com>
2020-12-24 15:20:18 +0200

    init

---
+++ app/src/main/java/pavelpoley/myapplication/MainActivity.kt
@@ -0,0 +1,32 @@
+package pavelpoley.myapplication
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import pavelpoley.myapplication.ui.home.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, HomeFragment(), null)
+                .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+}
---
+++ app/src/main/java/pavelpoley/myapplication/ui/home/HomeFragment.kt
@@ -0,0 +1,51 @@
+package pavelpoley.myapplication.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import pavelpoley.myapplication.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val rv: RecyclerView = view.findViewById(R.id.rv)
+        rv.layoutManager = LinearLayoutManager(context)
+        rv.adapter = Adapter()
+    }
+
+    class Adapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.row, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            //ignore
+        }
+
+        override fun getItemCount(): Int {
+            return 20
+        }
+
+        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    }
+}
